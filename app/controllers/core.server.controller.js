508e9e00af8e21db425beedd7539070de9e7607f
cecilia-donnelly <donnelly.cecilia1@gmail.com>
2015-08-06 14:14:20 -0500

    Remove obsolete "upload" route and controller: #25
    I added a new controller and route for uploads in commit b34aeba.
    However, as of commit cc7fdb2, we're processing the file on the browser
    side, so there's no longer any need for the "upload" route.  The file is
    never uploaded.  This commit just removes that no-longer-needed code.

--- app/controllers/core.server.controller.js
+++ app/controllers/core.server.controller.js
@@ -4,28 +4,8 @@
  * Module dependencies.
  */
 exports.index = function(req, res) {
-    if (req.method == "POST"){
-        var body = '';
-        req.on('data', function (data) {
-            body += data;
-            if (body.length > 1e6) { 
-                req.connection.destroy();
-            }
-        });
-        req.on('end', function () {
-            // naming this "file_data" because I assume that we will
-            // only be POST-ing imported files.  This may change in the
-            // future.
-            // split out the contents from the headers and footers
-            var csv_body = body.split("\r\n");
-            var csv_contents = csv_body[4];
-            res.send(csv_contents);
-        });
-    }
-    else{
         res.render('index', {
             user: req.user || null,
             request: req
         });
-    }
 };
